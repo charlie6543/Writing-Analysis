5a6321bd35ee728334370244b6833ba1570f66a5
charlie6543 <sli34@uw.edu>
2025-01-17 17:40:15 -0800

    adding highlight support to differentiate sentence lengths upon button press.

--- app.py
+++ app.py
@@ -1,16 +1,35 @@
 import os
 
 from flask import Flask, request, redirect, url_for, render_template, json
+from document import Document
+import re
 
 app = Flask(__name__)
 
 @app.route("/", methods=["POST", "GET"])
 def init_interface():
-  print('hi')
   return render_template('home.html')
 
 @app.route('/process', methods=['POST'])
 def process():
   data = request.form.get('data')
-  print(data)
-  return str(data)
+  '''doc = [[]]
+  print("hello")
+  frags = re.split(r"[.!?,:;-]", data)
+  punc = []
+  tracker = 0
+  for frag in frags:
+    tracker += len(frag)
+    if(tracker >= len(data)): 
+      break
+    print(data[tracker])
+    punc.append(data[tracker])
+    tracker += 1
+  doc.append(frags)
+  doc.append(punc)
+  return doc'''
+
+  doc = Document(str(data))
+  wordCounts = doc.getWords()
+  print(len(wordCounts))
+  return wordCounts
