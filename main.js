d419cd55919e117e7fb5323dfd6b621d78918e1e
charlie6543 <sli34@uw.edu>
2025-03-20 14:25:46 -0700

    adding onleave prompt, added undo button

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ BUGS:
 when re-analyzing, words/letters get eaten
 
 PLAN:
-change color variation so it makes more sense for things like paragraphs
 
 add settings + buttons to change analysis
 fix bugs when re-analyzing
@@ -26,9 +25,22 @@ highlighting text to only analyze that section
 */
 //import key from './env.js';
 //console.log(key);
-
+let colors = ['rgb(213, 236, 184)', 'rgb(184, 236, 206)', 
+    'rgb(184, 236, 235)', 'rgb(184, 204, 236)', 
+    'rgb(189, 184, 236)', 'rgb(214, 184, 236)', 
+    'rgb(248, 171, 255)', 'rgb(252, 161, 228)',
+    'rgb(237, 155, 182)', 'rgb(245, 167, 168)',
+    'rgb(245, 187, 146)', 'rgb(245, 222, 146)',
+    'rgb(232, 245, 146)' ]; // 13, 12 with 0 indexing
+let prevDoc = "";
 document.getElementById("submit").addEventListener("click", runAnalysis);
 document.getElementById("picker").addEventListener("change", unhide);
+document.getElementById("editor").addEventListener("keydown", function() {
+    prevDoc = document.getElementById("editor").innerHTML;
+})
+window.addEventListener("beforeunload", function(event) {
+    event.preventDefault();
+});
 
 function format(command, value) {
     document.execCommand(command, false, value);
@@ -39,6 +51,10 @@ function changeSize(){
     document.execCommand('fontSize', false, size);
 }
 
+function undo(){
+    if(prevDoc != "")
+        document.getElementById("editor").innerHTML = prevDoc;
+}
 
 class Document {
     constructor(body){
@@ -335,6 +351,7 @@ function openGraphs(event, tabName){
 function runAnalysis(){
     let docVal = document.getElementById('editor');
     let value = docVal.textContent;
+    prevDoc = value;
     let doc = new Document(docVal);
     updateCharts(doc);
     let highlightBy = document.getElementById("picker").value;
@@ -347,29 +364,78 @@ function runAnalysis(){
 }
 function unhide(){
     if(document.getElementById("picker").value == "dup"){
+        // document.getElementById("caseLabel").innerText = "Check for duplicate words within";
+        // document.getElementById("case")
         document.getElementById("dupPicker").classList.remove("hidden");
+        //document.getElementById("casePicker").classList.add("hidden");
     }
     else{
         document.getElementById("dupPicker").classList.add("hidden");
+        //document.getElementById("casePicker").classList.remove("hidden");
     }
 }
 
 // TODO: change cases for each color
 // depending on highlightBy/ user settings
+let paraCases = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+let sentCases = [2, 5, 8, 11, 14, 17, 20, 23, 27, 30, 33, 36, 39];
+let fragCases = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26];
+let wordCases = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
 function addColors(value, doc, highlightBy){
     let offset = 0;
     let newDoc = "";
+    let cases = [];
     for(let i = 0; i < doc.paragraphs.length; i++){
         let para = "<div>";
         let analysis = [];
         switch(highlightBy){
             case "paragraph": 
-                // TODO
                 analysis = "";
                 let color = '<mark style="background-color: ';
                 let cur = doc.paragraphs[i];
                 let sentLength = cur.length;
-                switch(sentLength){
+                if(sentLength <= paraCases[0]){
+                    color += colors[0];
+                }
+                else if(sentLength <= paraCases[1]){
+                    color += colors[1];
+                }
+                else if(sentLength <= paraCases[2]){
+                    color += colors[2];
+                }
+                else if(sentLength <= paraCases[3]){
+                    color += colors[3];
+                }
+                else if(sentLength <= paraCases[4]){
+                    color += colors[4];
+                }
+                else if(sentLength <= paraCases[5]){
+                    color += colors[5];
+                }
+                else if(sentLength <= paraCases[6]){
+                    color += colors[6];
+                }
+                else if(sentLength <= paraCases[7]){
+                    color += colors[7];
+                }
+                else if(sentLength <= paraCases[8]){
+                    color += colors[8];
+                }
+                else if(sentLength <= paraCases[9]){
+                    color += colors[9];
+                }
+                else if(sentLength <= paraCases[10]){
+                    color += colors[10];
+                }
+                else if(sentLength <= paraCases[11]){
+                    color += colors[11];
+                }
+                else{
+                    color += colors[12];
+                }
+
+
+                /*switch(sentLength){
                     case 0: case 1: case 2: case 3: case 4: case 5:
                         color += "rgb(213, 236, 184)";
                         break;
@@ -390,7 +456,7 @@ function addColors(value, doc, highlightBy){
                         break;
                     default:
                         color += "rgb(248, 171, 255)";
-                }
+                }*/
                 color += ';">';
                 // add to start of sentence
                 let start = cur.pos + offset;
@@ -401,12 +467,15 @@ function addColors(value, doc, highlightBy){
                 break;
             case "sentence":
                 analysis = doc.paragraphs[i].sentences;
+                cases = sentCases;
                 break;
             case "frag":
                 analysis = doc.paragraphs[i].getFragments();
+                cases = fragCases;
                 break;
             case "words":
                 analysis = doc.paragraphs[i].getWords();
+                cases = wordCases;
                 break;
         }
         // console.log(`${analysis}`);
@@ -414,28 +483,67 @@ function addColors(value, doc, highlightBy){
             let color = '<mark style="background-color: ';
             let cur = analysis[n];
             let sentLength = cur.length;
-            switch(sentLength){
-                case 0: case 1: case 2: case 3: case 4: case 5:
-                    color += "rgb(213, 236, 184)";
-                    break;
-                case 6: case 7: case 8:
-                    color += "rgb(184, 236, 206)";
-                    break;
-                case 9: case 10: case 11:
-                    color += "rgb(184, 236, 235)";
-                    break;
-                case 12: case 13: case 14:
-                    color += "rgb(184, 204, 236)";
-                    break;
-                case 15: case 16: case 17:
-                    color += "rgb(189, 184, 236)";
-                    break;
-                case 18: case 19: case 20:
-                    color += "rgb(214, 184, 236)";
-                    break;
-                default:
-                    color += "rgb(248, 171, 255)";
+            if(sentLength <= cases[0]){
+                color += colors[0];
             }
+            else if(sentLength <= cases[1]){
+                color += colors[1];
+            }
+            else if(sentLength <= cases[2]){
+                color += colors[2];
+            }
+            else if(sentLength <= cases[3]){
+                color += colors[3];
+            }
+            else if(sentLength <= cases[4]){
+                color += colors[4];
+            }
+            else if(sentLength <= cases[5]){
+                color += colors[5];
+            }
+            else if(sentLength <= cases[6]){
+                color += colors[6];
+            }
+            else if(sentLength <= cases[7]){
+                color += colors[7];
+            }
+            else if(sentLength <= cases[8]){
+                color += colors[8];
+            }
+            else if(sentLength <= cases[9]){
+                color += colors[9];
+            }
+            else if(sentLength <= cases[10]){
+                color += colors[10];
+            }
+            else if(sentLength <= cases[11]){
+                color += colors[11];
+            }
+            else{
+                color += colors[12];
+            }
+            // switch(sentLength){
+            //     case 0: case 1: case 2: case 3: case 4: case 5:
+            //         color += "rgb(213, 236, 184)";
+            //         break;
+            //     case 6: case 7: case 8:
+            //         color += "rgb(184, 236, 206)";
+            //         break;
+            //     case 9: case 10: case 11:
+            //         color += "rgb(184, 236, 235)";
+            //         break;
+            //     case 12: case 13: case 14:
+            //         color += "rgb(184, 204, 236)";
+            //         break;
+            //     case 15: case 16: case 17:
+            //         color += "rgb(189, 184, 236)";
+            //         break;
+            //     case 18: case 19: case 20:
+            //         color += "rgb(214, 184, 236)";
+            //         break;
+            //     default:
+            //         color += "rgb(248, 171, 255)";
+            // }
             color += ';">';
             // add to start of sentence
             let start = cur.pos + offset;
@@ -458,54 +566,115 @@ function addColors(value, doc, highlightBy){
 
 
 function updateCharts(body){
-    updateChart(wordPerSentChart, body.getWordsPerSentence());
-    updateChart(letterPerWordChart, body.getLettersPerWord());
-    updateChart(sentPerParaChart, body.getSentencesPerParagraph());
-    updateChart(wordPerParaChart, body.getWordsPerParagraph());
-    updateChart(fragPerParaChart, body.getFragsPerParagraph());
-    updateChart(fragPerSentChart, body.getFragsPerSentence());
-    updateChart(wordPerFragChart, body.getWordsPerFrag());
+    updateChart(wordPerSentChart, body.getWordsPerSentence(), "wordPerSent");
+    updateChart(letterPerWordChart, body.getLettersPerWord() , "letterPerWord");
+    updateChart(sentPerParaChart, body.getSentencesPerParagraph(), "sentencePerPara");
+    updateChart(wordPerParaChart, body.getWordsPerParagraph(), "wordPerPara");
+    updateChart(fragPerParaChart, body.getFragsPerParagraph(), "fragPerPara");
+    updateChart(fragPerSentChart, body.getFragsPerSentence(), "fragPerSent");
+    updateChart(wordPerFragChart, body.getWordsPerFrag(), "wordPerFrag");
 }
 
-function updateChart(chart, data){
+function updateChart(chart, data, type){
     chart.data.labels = data;
     chart.data.datasets.forEach((dataset) => {dataset.data = data});
-    let colors = [];
-    for(let i = 0; i < data.length; i++){
-        switch(data[i]){
-            case 0: case 1: case 2: case 3: case 4: case 5:
-                colors.push("rgb(213, 236, 184)");
-                break;
-            case 6: case 7: case 8:
-                colors.push("rgb(184, 236, 206)");
-                break;
-            case 9: case 10: case 11:
-                colors.push("rgb(184, 236, 235)");
-                break;
-            case 12: case 13: case 14:
-                colors.push("rgb(184, 204, 236)");
-                break;
-            case 15: case 16: case 17:
-                colors.push("rgb(189, 184, 236)");
-                break;
-            case 18: case 19: case 20:
-                colors.push("rgb(214, 184, 236)");
-                break;
-            default:
-                colors.push("rgb(248, 171, 255)");
-        }
+    let graphColors = [];
+    let cases = [];
+    switch(type){
+        case "wordPerSent":
+            cases = sentCases;
+            break;
+        case "letterPerWord":
+            cases = wordCases;
+            break;
+        case "sentencePerPara":
+            cases = paraCases;
+            break;
+        case "wordPerPara":
+            cases = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130];
+            break;
+        case "fragPerPara":
+            cases = wordCases;
+            break;
+        case "fragPerSent":
+            cases = wordCases;
+            break;
+        case "wordPerFrag":
+            cases = fragCases;
+            break;
+        default: // letterPerWord
+            cases = [2, 5, 8, 11, 14, 17, 20, 23, 27, 30, 33, 36, 39];
     }
-    chart.data.datasets.forEach((dataset) => {dataset.backgroundColor = colors});
+
+    //console.log(type + ": " + cases);
+    for(let i = 0; i < data.length; i++){
+        if(data[i] <= cases[0]){
+            graphColors.push(colors[0]);
+        }
+        else if(data[i] <= cases[1]){
+            graphColors.push(colors[1]);
+        }
+        else if(data[i] <= cases[2]){
+            graphColors.push(colors[2]);
+        }
+        else if(data[i] <= cases[3]){
+            graphColors.push(colors[3]);
+        }
+        else if(data[i] <= cases[4]){
+            graphColors.push(colors[4]);
+        }
+        else if(data[i] <= cases[5]){
+            graphColors.push(colors[5]);
+        }
+        else if(data[i] <= cases[6]){
+            graphColors.push(colors[6]);
+        }
+        else if(data[i] <= cases[7]){
+            graphColors.push(colors[7]);
+        }
+        else if(data[i] <= cases[8]){
+            graphColors.push(colors[8]);
+        }
+        else if(data[i] <= cases[9]){
+            graphColors.push(colors[9]);
+        }
+        else if(data[i] <= cases[10]){
+            graphColors.push(colors[10]);
+        }
+        else if(data[i] <= cases[11]){
+            graphColors.push(colors[11]);
+        }
+        else{
+            graphColors.push(colors[12]);
+        }
+        // switch(data[i]){
+        //     case 0: case 1: case 2: case 3: case 4: case 5:
+        //         colors.push("rgb(213, 236, 184)");
+        //         break;
+        //     case 6: case 7: case 8:
+        //         colors.push("rgb(184, 236, 206)");
+        //         break;
+        //     case 9: case 10: case 11:
+        //         colors.push("rgb(184, 236, 235)");
+        //         break;
+        //     case 12: case 13: case 14:
+        //         colors.push("rgb(184, 204, 236)");
+        //         break;
+        //     case 15: case 16: case 17:
+        //         colors.push("rgb(189, 184, 236)");
+        //         break;
+        //     case 18: case 19: case 20:
+        //         colors.push("rgb(214, 184, 236)");
+        //         break;
+        //     default:
+        //         colors.push("rgb(248, 171, 255)");
+        // }
+    }
+    chart.data.datasets.forEach((dataset) => {dataset.backgroundColor = graphColors});
     chart.update();
 }
 
-let colors = ['rgb(213, 236, 184)', 'rgb(184, 236, 206)', 
-    'rgb(184, 236, 235)', 'rgb(184, 204, 236)', 
-    'rgb(189, 184, 236)', 'rgb(214, 184, 236)', 
-    'rgb(248, 171, 255)', 'rgb(252, 161, 228)',
-    'rgb(237, 155, 182)', 'rgb(245, 167, 168)',
-    'rgb(245, 187, 146)', 'rgb(245, 222, 146)',
-    'rgb(232, 245, 146)' ];
+
 function findDup(doc, prox){
     // look for duplicates within prox
     // maintain dict of words
